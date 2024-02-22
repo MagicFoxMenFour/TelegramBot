3fc690203bff70914e3a098bf0cd22aaaf4e3a8a
MFMF <p0p0vk@yandex.ru>
2024-02-22 23:40:15 +0300

    Токен и запуск бота

---
+++ Tg_aio3/run.py
@@ -0,0 +1,20 @@
+from aiogram import Bot, Dispatcher
+from config import TOKEN
+from app.handlers import router
+import logging
+import sys
+import asyncio
+
+async def main():
+    bot = Bot(token=TOKEN)
+    dp = Dispatcher()
+    await dp.start_polling(bot)
+    
+    
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO, stream=sys.stdout)
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        print('Exit')
+        
